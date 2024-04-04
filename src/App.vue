10664fe0ea7c384afa171316b4a8f2d97d005f67
LaScheggia <lascheggia605@gmail.com>
2024-04-04 18:35:55 +0200

    removed comment

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,8 @@
 </template>
 
 <script setup>
-import NavBar from './components/NavBar.vue'; // Import your NavBar component here
-import Footer from './components/Footer.vue'; // Import your Footer component here
+import NavBar from './components/NavBar.vue'; 
+import Footer from './components/Footer.vue'; 
 
 
 import { useAuthStore } from '@/stores';
